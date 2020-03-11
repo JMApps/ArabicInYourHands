06684889438c12ab8d4fe7d665b65162b9e00919
dev_muslim <jmappsmuslim@gmail.com>
2020-03-11 09:12:35 +0300

    Added splash screen

---
+++ app/src/main/kotlin/jmapps/arabicinyourhands/ui/activities/SplashActivity.kt
@@ -0,0 +1,16 @@
+package jmapps.arabicinyourhands.ui.activities
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val toMainActivity = Intent(this, MainActivity::class.java)
+        startActivity(toMainActivity)
+        finish()
+    }
+}
