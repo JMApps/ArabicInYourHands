af45063f65d9be86927707395ee961233c4d3c82
dev_muslim <jmappsmuslim@gmail.com>
2020-03-11 15:23:29 +0300

    Added content for other mvp

--- app/src/main/kotlin/jmapps/arabicinyourhands/mvp/other/OtherContract.kt
+++ app/src/main/kotlin/jmapps/arabicinyourhands/mvp/other/OtherContract.kt
@@ -6,9 +6,29 @@ interface OtherContract {
 
     interface OtherView {
         fun replaceFragment(fragment: Fragment)
+
+        fun getTools()
+
+        fun getNightMode(state: Boolean)
+
+        fun isNightMode(state: Boolean)
+
+        fun getAboutUs()
     }
 
     interface OtherPresenter {
         fun replaceFragment(fragment: Fragment)
+
+        fun setTools()
+
+        fun setNightMode(state: Boolean)
+
+        fun setAboutUs()
+
+        fun otherApp()
+
+        fun rateApp()
+
+        fun shareLink()
     }
 }
--- app/src/main/kotlin/jmapps/arabicinyourhands/mvp/other/OtherPresenterImpl.kt
+++ app/src/main/kotlin/jmapps/arabicinyourhands/mvp/other/OtherPresenterImpl.kt
@@ -1,11 +1,50 @@
 package jmapps.arabicinyourhands.mvp.other
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.fragment.app.Fragment
+import jmapps.arabicinyourhands.R
 
-class OtherPresenterImpl(private val otherView: OtherContract.OtherView?) :
+class OtherPresenterImpl(private val context: Context?, private val otherView: OtherContract.OtherView?) :
     OtherContract.OtherPresenter {
+
+    private val linkDescription = context?.getString(R.string.app_name)
+    private val linkOtherApp = "https://telegra.ph/JM-Applications-03-11"
+    private val linkApp = "https://play.google.com/store/apps/details?id=jmapps.arabicinyourhands"
 
     override fun replaceFragment(fragment: Fragment) {
         otherView?.replaceFragment(fragment)
+    }
+
+    override fun setTools() {
+        otherView?.getTools()
+    }
+
+    override fun setNightMode(state: Boolean) {
+        otherView?.getNightMode(state)
+    }
+
+    override fun setAboutUs() {
+        otherView?.getAboutUs()
+    }
+
+    override fun otherApp() {
+        val rate = Intent(Intent.ACTION_VIEW)
+        rate.data = Uri.parse(linkOtherApp)
+        context?.startActivity(rate)
+    }
+
+    override fun rateApp() {
+        val rate = Intent(Intent.ACTION_VIEW)
+        rate.data = Uri.parse(linkApp)
+        context?.startActivity(rate)
+    }
+
+    override fun shareLink() {
+        val shareLink = Intent(Intent.ACTION_SEND)
+        shareLink.type = "text/plain"
+        shareLink.putExtra(Intent.EXTRA_TEXT, "$linkDescription\n$linkApp")
+        context?.startActivity(shareLink)
     }
 }
