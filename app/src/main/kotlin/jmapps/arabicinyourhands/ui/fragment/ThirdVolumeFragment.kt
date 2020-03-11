efbc85b20d48caaa5270f361b5a3996baaa2277a
dev_muslim <jmappsmuslim@gmail.com>
2020-03-11 09:10:35 +0300

    Added fragments with view model

---
+++ app/src/main/kotlin/jmapps/arabicinyourhands/ui/fragment/ThirdVolumeFragment.kt
@@ -0,0 +1,34 @@
+package jmapps.arabicinyourhands.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import jmapps.arabicinyourhands.R
+import jmapps.arabicinyourhands.databinding.FragmentThirdVolumeBinding
+import jmapps.arabicinyourhands.ui.viewmodel.MainChaptersViewModel
+
+class ThirdVolumeFragment : Fragment() {
+
+    private lateinit var binding: FragmentThirdVolumeBinding
+    private lateinit var mainChapterViewModel: MainChaptersViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainChapterViewModel = ViewModelProvider(this).get(MainChaptersViewModel::class.java).apply {
+            setIndex(3)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_third_volume, container, false)
+        mainChapterViewModel.texts.observe(viewLifecycleOwner, Observer<String> {
+
+        })
+        return binding.root
+    }
+}
