5459051cf55f99b969be3fa294b809a1bb49453a
dev_muslim <jmappsmuslim@gmail.com>
2020-03-18 15:51:56 +0300

    Added lock orientation activity

--- app/src/main/kotlin/jmapps/arabicinyourhands/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/jmapps/arabicinyourhands/ui/activities/MainActivity.kt
@@ -43,6 +43,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.mainAppBar.toolbarMain)
 
+        LockOrientation(this).lock()
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this)
         editor = preferences.edit()
 
