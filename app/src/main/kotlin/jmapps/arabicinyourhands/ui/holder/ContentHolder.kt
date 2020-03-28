cc1ba070433d1941597ddc9ac1069acd4374790c
dev_muslim <jmappsmuslim@gmail.com>
2020-03-28 09:57:23 +0300

    Added logic tools

--- app/src/main/kotlin/jmapps/arabicinyourhands/ui/holder/ContentHolder.kt
+++ app/src/main/kotlin/jmapps/arabicinyourhands/ui/holder/ContentHolder.kt
@@ -1,13 +1,21 @@
 package jmapps.arabicinyourhands.ui.holder
 
+import android.content.SharedPreferences
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.RecyclerView
 import jmapps.arabicinyourhands.R
+import jmapps.arabicinyourhands.data.file.TypeFace
 import jmapps.arabicinyourhands.ui.adapter.ContentAdapter
+import jmapps.arabicinyourhands.ui.fragment.ToolsBottomSheet
 
-class ContentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ContentHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private var preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(itemView.context)
+    private var textSizeValues = (16..30).toList().filter { it % 2 == 0 }
 
     val llContentItem: LinearLayout = itemView.findViewById(R.id.llContentItem)
     val tvArabicName: TextView = itemView.findViewById(R.id.tvArabicName)
@@ -15,9 +23,104 @@ class ContentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val tvTranslationName: TextView = itemView.findViewById(R.id.tvTranslationName)
     val tvTranslationContent: TextView = itemView.findViewById(R.id.tvTranslationContent)
 
+    init {
+        PreferenceManager.getDefaultSharedPreferences(itemView.context).registerOnSharedPreferenceChangeListener(this)
+        textSizes()
+        textArabicFont()
+        textTranslationFont()
+        textsShowState()
+    }
+
     fun findItemClick(onContentItemClick: ContentAdapter.OnContentItemClick, contentId: Int, contentPosition: Int) {
         itemView.setOnClickListener {
             onContentItemClick.onItemClick(contentId, contentPosition)
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        textSizes()
+        textArabicFont()
+        textTranslationFont()
+        textsShowState()
+    }
+
+    private fun textSizes() {
+        val arabicProgress = preferences.getInt(ToolsBottomSheet.ArabicTextSize, 1)
+        val translationProgress = preferences.getInt(ToolsBottomSheet.TranslationTextSize, 1)
+
+        tvArabicName.textSize = textSizeValues[arabicProgress].toFloat()
+        tvArabicContent.textSize = textSizeValues[arabicProgress].toFloat()
+
+        tvTranslationName.textSize = textSizeValues[translationProgress].toFloat()
+        tvTranslationContent.textSize = textSizeValues[translationProgress].toFloat()
+    }
+
+    private fun textArabicFont() {
+        val arabicFontOne = preferences.getBoolean(ToolsBottomSheet.ArabicFontOne, true)
+        val arabicFontTwo = preferences.getBoolean(ToolsBottomSheet.ArabicFontTwo, false)
+        val arabicFontThree = preferences.getBoolean(ToolsBottomSheet.ArabicFontThree, false)
+
+        when(true) {
+
+            arabicFontOne -> {
+                arabicFont("fonts/droid_naskh.ttf")
+            }
+
+            arabicFontTwo -> {
+                arabicFont("fonts/quran_font.ttf")
+            }
+
+            arabicFontThree -> {
+                arabicFont("fonts/uthmanic.ttf")
+            }
+        }
+    }
+
+    private fun arabicFont(font: String) {
+        tvArabicName.typeface = TypeFace()[itemView.context!!, font]
+        tvArabicContent.typeface = TypeFace()[itemView.context!!, font]
+    }
+
+    private fun textTranslationFont() {
+        val translationFontOne = preferences.getBoolean(ToolsBottomSheet.TranslationFontOne, true)
+        val translationFontTwo = preferences.getBoolean(ToolsBottomSheet.TranslationFontTwo, false)
+        val translationFontThree = preferences.getBoolean(ToolsBottomSheet.TranslationFontThree, false)
+
+        when(true) {
+
+            translationFontOne -> {
+                translationFont("fonts/gilroy.ttf")
+            }
+
+            translationFontTwo -> {
+                translationFont("fonts/cambria_medium.ttf")
+            }
+
+            translationFontThree -> {
+                translationFont("fonts/serif.ttf")
+            }
+        }
+    }
+
+    private fun translationFont(font: String) {
+        tvTranslationName.typeface = TypeFace()[itemView.context!!, font]
+        tvTranslationContent.typeface = TypeFace()[itemView.context!!, font]
+    }
+
+    private fun textsShowState() {
+        val switchArabicState = preferences.getBoolean(ToolsBottomSheet.SwitchArabicShow, true)
+        val switchTranslationState = preferences.getBoolean(ToolsBottomSheet.SwitchTranslationShow, true)
+
+        if (switchArabicState) {
+            tvArabicContent.visibility = View.VISIBLE
+        } else {
+            tvArabicContent.visibility = View.INVISIBLE
+        }
+
+        if (switchTranslationState) {
+            tvTranslationContent.visibility = View.VISIBLE
+        } else {
+            tvTranslationContent.visibility = View.INVISIBLE
         }
     }
 }
