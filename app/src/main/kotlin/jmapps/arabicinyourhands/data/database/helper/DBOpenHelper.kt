7bfdc48f2f00d0360292244606fe472420e9610e
dev_muslim <jmappsmuslim@gmail.com>
2020-04-12 09:25:59 +0300

    Fixes data base

--- app/src/main/kotlin/jmapps/arabicinyourhands/data/database/helper/DBOpenHelper.kt
+++ app/src/main/kotlin/jmapps/arabicinyourhands/data/database/helper/DBOpenHelper.kt
@@ -3,12 +3,12 @@ package jmapps.arabicinyourhands.data.database.helper
 import android.content.Context
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
-private const val dbVersion = 1
+private const val dbVersion = 2
 
 class DBOpenHelper(private val context: Context) :
     SQLiteAssetHelper(context, "ArabicInDB", null, dbVersion) {
 
     init {
-        setForcedUpgrade(dbVersion)
+        setForcedUpgrade()
     }
 }
